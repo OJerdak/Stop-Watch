ca04bdb3f4614c2a61283d77cc1f0bb58128f8a7
ojerdak-koein <ojerdak@koein.com>
2023-04-10 23:15:13 +0300

    first-commit

---
+++ StopWatch/Models/StopWatch.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StopWatch.Models
+{
+    public class Timer
+    {
+        public DateTime StartTime { get; set; }
+        public DateTime StopTime { get; set; }
+        public bool isRunning { get; set; }
+        public int Duration {
+            get {
+                var timeSpan = this.StopTime - this.StartTime;
+                var duration = timeSpan.TotalSeconds;
+                return Convert.ToInt32(duration);
+            } 
+        }
+
+        public void Start()
+        {
+            this.StartTime = DateTime.Now;
+        }
+        public void Stop()
+        {
+            this.StopTime = DateTime.Now;
+        }
+    }
+}
---
+++ StopWatch/Program.cs
@@ -0,0 +1,62 @@
+﻿using StopWatch.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StopWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my stopwatch project, you may exit anytime you wish by entering 'E'");
+            Console.WriteLine("Enter 'R' to run the stopwatch and 'S' to stop it :");
+
+            var sw = new Timer();
+
+            string input = "";
+
+            while (input != "E")
+            {
+                input = Console.ReadLine();
+
+                try
+                {
+                    if (input == "R" && !sw.isRunning)
+                    {
+                        sw.Start();
+                        sw.isRunning = true;
+                        Console.WriteLine("The timer is running press 'S' to stop it");
+                    }
+                    else if (input == "R" && sw.isRunning)
+                    {
+                        throw new InvalidOperationException("There's an existing instance of the timer already running");
+                    }
+                    else if (input == "S" && sw.isRunning)
+                    {
+                        sw.Stop();
+                        sw.isRunning = false;
+                        Console.WriteLine("Timer has stopped, total duration is : {0} seconds", sw.Duration);
+                    }
+                    else if (input == "S" && !sw.isRunning)
+                    {
+                        sw.Stop();
+                        throw new InvalidOperationException("There's no existing instance of the timer already running");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter a valid character");
+                    }
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);                    
+                }
+
+            }
+
+        }
+    }
+}
